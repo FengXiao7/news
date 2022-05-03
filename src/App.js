54ee8eb5f576013418c9a8c3b249a5147459f928
冯成祥 <18284557828@163.com>
2022-05-03 14:24:27 +0800

    初始化

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import IndexRouter from './router/IndexRouter'
+import './App.css'
+
+function App() {
+  return <div>
+    <IndexRouter></IndexRouter>
+  </div>
+}
+export default App
---
+++ src/views/sandbox/home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from 'antd'
+
+export default function Home() {
+    return (
+        <div>
+            home
+            <Button type="primary">Primary Button</Button>
+        </div>
+    )
+}
